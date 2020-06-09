356f19375fa9c4442bd4b4dac2424684b0ad64b3
pjenkinsondev <61879828+pjenkinsondev@users.noreply.github.com>
2020-06-09 12:30:48 +0100

    Git ignore fix

--- devkit-v5/assets/v4-js/head.src.js
+++
@@ -1,8 +0,0 @@
-/* v4.2.7 */
-
-/* Placeholder for upcoming scripts which need to be in the head eg HTML5Shiv/Modernizr */
-
-/* jQuery is loaded in gutter - Don't put Javascript which needs jQuery in here! */
-
-// Fixes < IE6's broken support for the ABBR tag abbr bugs but it needs calling before the DOM is loaded 
-document.createElement("abbr"); 
