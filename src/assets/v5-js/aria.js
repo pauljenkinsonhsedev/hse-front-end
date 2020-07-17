7d5b13f6f5bad019b22f4b0bccea10941125effc
pjenkinsondev <61879828+pjenkinsondev@users.noreply.github.com>
2020-07-17 06:55:06 +0100

    CSS update
    CSS link style updated for accessibility, includes underlining of links for all lists.

--- src/assets/v5-js/aria.js
+++ src/assets/v5-js/aria.js
@@ -1,14 +1,18 @@
 // V5 Aria roles
 
 $(document).ready(function () {
-  $('#article').attr('role','article');
   $('#breadCrumb').attr('aria-label','Previous pages:');
+  $('#cookieContainer').attr('aria-label','Cookie banner');
+  $('#printBanner').attr('aria-label','Print banner');
+  $('#lastUpdated').attr('aria-label','Last updated');
+  $('#top').attr('aria-label','Skip to content');
+  $('#beta-user-testing').attr('aria-label','Beta feedback');
+  $('#feedback').attr('aria-label','Is this page useful');
   $('p.feedback-message').attr('aria-described','Yes, link, is this page useful?');
   $('#breadCrumb li:last-of-type').attr('aria-current','page');
   $('#pagination')
 	.attr('role','navigation')
 	.attr('aria-label','Pagination Navigation');
-  $('#asideBottom').attr('role','complementary');
   $('#multistepNav').attr('role','navigation').attr('aria-label','Secondary');
 });
 
