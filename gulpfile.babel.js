c21e7139030ba133cc291e2028d58e0da6b350fb
Peter Broom <me@peterbroom.co.uk>
2020-08-12 11:31:57 +0100

    remove comments

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -14,24 +14,6 @@ import requireDir from 'require-dir';
 // index all gulp tasks
 requireDir('./gulp/tasks', { recurse: true });
 
-// const watcher = watch(config.all, series(['workspaceStyles']));
-
-// const time = format.asString('hh:mm:ss', new Date());
-// const timestamp = "[".white + time.grey + "]".white;
-
-// watcher.on('change', function(path) {
-//     console.log(`${timestamp} File ${path.yellow} was changed`);
-// });
-
-// watcher.on('add', function(path) {
-//     console.log(`${timestamp} File ${path.green} was added`);
-// });
-
-// watcher.on('unlink', function(path) {
-//     console.log(`${timestamp} File ${path.red} was removed`);
-// });
-
-// watchTask(config.all);
 // Default task
 task('default', series(hseBuild, commonTasks));
 
