680b9569518194c68b8e15aed0da2a7e7e9c580e
Peter Broom <me@peterbroom.co.uk>
2020-08-26 13:30:36 +0100

    table attribute change and descriptive commets

--- src/shared/js/combined/charts/options.js
+++ src/shared/js/combined/charts/options.js
@@ -1,4 +1,19 @@
-import { ChartOptionsDefault, ChartOptionsArearange, ChartOptionsPie, ChartOptionsBarStacked } from './dependencies';
+/*
+    Class @ChartOptions
+
+    Description:
+    - foundation for all HighCharts options
+    - loops through charts to determine their options
+    - imports options for various chart types
+
+    Usage:
+    chartArray.forEach((container) => {
+        let defaultOptions = new ChartOptions(container);
+        ...build charts function here
+    });
+*/
+
+import { ChartOptionsDefault, ChartOptionsLine, ChartOptionsArearange, ChartOptionsPie, ChartOptionsBarStacked } from './dependencies';
 
 export class ChartOptions {
     constructor(container) {
@@ -10,8 +25,8 @@ export class ChartOptions {
         this.title = container.dataset.chartTitle;
         this.subtitle = container.dataset.chartSubtitle;
         this.description = container.querySelector('.datacontent__description');
-        this.tableDescYText = container.dataset.tableDescY;
-        this.tableDescXText = container.dataset.tableDescX;
+        this.yAxisText = container.dataset.yaxisText;
+        this.xAxisText = container.dataset.xaxisText;
         this.units = container.dataset.chartUnits;
         this.colWidth = 75;
         this.dataLabelsSuffix = this.displaySuffix(this.units);
@@ -19,14 +34,20 @@ export class ChartOptions {
         this.defaults = new Array;
         this.collection = new Array;
 
+        /*
+            Plot events boolean
+            - diables click events if only one set of data
+        */
+        this.plotEvents = false;
+        for (let row of this.dataTable.rows) {
+            const cells = row.querySelectorAll('td');
+            if (cells.length > 2) {
+                this.plotEvents = true;
+            }
+        }
+
         this.defaults = {
             chart: {
-                spacingBottom: 20,
-                spacingTop: 20,
-                spacingLeft: 20,
-                spacingRight: 20,
-                borderColor: '#E1E1E1',
-                borderWidth: 1,
                 type: this.type,
                 renderTo: this.chartRender
             },
@@ -54,13 +75,12 @@ export class ChartOptions {
             xAxis: [{
                 categories: this.categories,
                 title: {
-                    text: this.tableDescXText
+                    text: this.xAxisText
                 },
                 accessibility: {
                     description: this.title
                 },
                 labels: {
-                    // here
                     format: this.dataLabelsSuffix,
                     // formatter: function () {
 
@@ -70,14 +90,29 @@ export class ChartOptions {
                     // }
                 }
             }],
-
-            // series: [{
-            //     name: 'Temperature',
-            //     data: this.dataTable,
-            //     tooltip: {
-            //     valueSuffix: this.dataLabelsSuffix
-            // },
-            // }],
+            plotOptions: {
+                series: {
+                    showInLegend: true,
+                    events: {
+                        legendItemClick: () => {
+                            return this.plotEvents;
+                        }
+                    }
+                },
+                column: {
+                    maxPointWidth: this.colWidth
+                }
+            },
+            legend: {
+                enabled: true,
+                itemStyle: {
+                    font: 'Arial, Helvetica, sans-serif',
+                    color: '#000'
+                },
+                itemHoverStyle:{
+                    color: 'gray'
+                }
+            },
             credits: {
                 enabled: false
             },
@@ -94,6 +129,10 @@ export class ChartOptions {
         switch(this.type) {
             case 'pie':
                 this.collection = new ChartOptionsPie(this.container);
+            break;
+
+            case 'line':
+                this.collection = new ChartOptionsLine(this.container);
             break;
 
             case 'barstacked':
@@ -136,10 +175,10 @@ export class ChartOptions {
             result = `<b>{point.name}</b>: {point.y}`
             break;
         case 'celsius':
-            result = `<b>{point.name}</b>: {point.y}°C`
+            result = `°C`
             break;
         case 'fahrenheit':
-            result = `<b>{point.name}</b>: {point.y}°F`
+            result = `°F`
             break;
         case 'date':
             // this.labelFormatter(this.units);
@@ -151,176 +190,3 @@ export class ChartOptions {
         return result;
     }
 }
-
-
-    // defaultChartOptions(getOptions, categories) {
-    //     let xAxis = [{
-    //         categories: categories,
-    //         title: {
-    //             text: this.tableDescXText
-    //         },
-    //         accessibility: {
-    //             description: this.title
-    //         },
-    //         labels: {
-    //             // here
-    //             // formatter: function () {
-
-    //             //     const date = moment(this.value).format('DD MMMM');
-    //             //     console.log(`formatDate ${date}`);
-    //             //     return date;
-    //             // }
-    //         }
-    //     }];
-    //     let yAxis = [{
-    //         title: {
-    //             text: this.tableDescYText,
-    //         },
-    //         accessibility: {
-    //             description: this.title
-    //         }
-    //     }];
-    //     let plotOptions = {
-    //         series: {
-    //             showInLegend: this.legend
-    //         },
-    //         column: {
-    //             maxPointWidth: 75
-    //         }
-    //     }
-
-    //     let tooltip = {
-    //         // shared: true
-    //     };
-
-    //     const options = { ...getOptions, plotOptions, xAxis, yAxis, tooltip};
-
-    //     return options;
-    // }
-
-    // barStackedOptions(getOptions, categories) {
-    //     let chart=  {
-    //         type: 'bar'
-    //     };
-    //     let xAxis = {
-    //         categories: categories
-    //     };
-    //     let yAxis = {
-    //         min: 0,
-    //         title: {
-    //             text: this.title
-    //         }
-    //     };
-    //     let legend = {
-    //         reversed: true
-    //     };
-    //     let plotOptions = {
-    //         series: {
-    //             stacking: 'normal'
-    //         }
-    //     };
-
-    //     const options = { ...getOptions, chart, xAxis, yAxis, legend, plotOptions};
-
-    //     return options;
-    // }
-
-    // areaRangeOptions(){
-    //     const seriesRangeData = seriesDataRanges(this.dataTable);
-    //     const averagesData = seriesRangeData.averagesData();
-    //     const rangesData = seriesRangeData.rangesData();
-
-    //     let title = {
-    //         text: 'July temperatures'
-    //     };
-
-    //     let tooltip = {
-    //         crosshairs: true,
-    //         shared: true,
-    //         valueSuffix: '°C'
-    //     };
-
-    //     let series = [{
-    //         name: 'Temperature',
-    //         data: averagesData[0],
-    //         zIndex: 1,
-    //         fillOpacity: 0.3,
-    //         color: this.brandColours[0],
-    //         marker: {
-    //             fillColor: 'white',
-    //             lineWidth: 2,
-    //             lineColor: this.brandColours[0]
-    //         }
-    //     }, {
-    //         name: 'Range',
-    //         data: rangesData[0],
-    //         type: 'arearange',
-    //         lineWidth: 0,
-    //         linkedTo: ':previous',
-    //         color: this.brandColours[0],
-    //         fillOpacity: 0.3,
-    //         zIndex: 0,
-    //         marker: {
-    //             enabled: false
-    //         }
-    //     }];
-
-    //     let xAxis = {
-    //         type: 'datetime',
-    //         accessibility: {
-    //             rangeDescription: 'Range: Jul 1st 2009 to Jul 31st 2009.'
-    //         }
-    //     }
-
-    //     let yAxis = {
-    //         title: {
-    //             text: null
-    //         }
-    //     }
-
-    //     let credits = {
-    //         enabled: false
-    //     };
-
-    //     let exporting = {
-    //         enabled: false
-    //     };
-
-    //     const options = {title, xAxis, yAxis, tooltip, series, credits, exporting};
-
-    //     return options;
-    // }
-
-    // pieChartOptions(getOptions){
-    //     let pieOptions = {
-    //         allowPointSelect: true,
-    //         cursor: 'pointer',
-    //         dataLabels: {
-    //             enabled: true,
-    //             format: this.dataLabelsSuffix,
-    //             connectorColor: 'silver'
-    //         }
-    //     }
-    //     let plotOptions = {
-    //         pie: {
-    //             allowPointSelect: true,
-    //             cursor: 'pointer',
-    //             dataLabels: {
-    //                 enabled: true,
-    //                 format: this.dataLabelsSuffix,
-    //                 connectorColor: 'silver'
-    //             }
-    //         }
-    //     }
-
-    //     const options = { ...getOptions, pieOptions, plotOptions};
-
-    //     return options;
-    // }
-
-
-// export default ChartOptions;
-
-    // labelFormatter(units){
-    //     console.log(`units ${units}`);
-    // }
