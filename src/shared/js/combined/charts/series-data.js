bbd697e32d11951b3d85450e5a43dfcafbb2a664
Peter Broom <me@peterbroom.co.uk>
2020-09-02 15:43:46 +0100

    series data from tables

--- src/shared/js/combined/charts/series-data.js
+++ src/shared/js/combined/charts/series-data.js
@@ -1,53 +1,73 @@
 export function seriesData(data) {
+    const thead = data.querySelector('.table__head');
     const tbody = data.querySelector('.table__body');
-/*
-    function @seriesData
+    /*
+        function @seriesData
 
-    Description: sets default series data for HighCharts options
+        Description: sets default series data for HighCharts options
 
-    usage:
+        usage:
 
-    const getSeriesData = seriesData(tableObject);
-    let series = getSeriesData[0]; // can then be pushed into options array
+        const getSeriesData = seriesData(tableObject);
+        let series = getSeriesData[0]; // can then be pushed into options array
 
-    Outputs something like...
+        Outputs something like...
 
-    series: [{
-        name: 'Tokyo',
-        data: [7.0, 6.9, 9.5, 14.5, 18.4, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
-    }, {
-        name: 'London',
-        data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
-    }]
-*/
+        series: [{
+            name: 'Tokyo',
+            data: [7.0, 6.9, 9.5, 14.5, 18.4, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+        }, {
+            name: 'London',
+            data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
+        }]
+    */
 
     // build data set
-    const seriesData = [].reduce.call(tbody.rows, function (seriesData, row) {
-        const cells = row.cells;
-        const category = [...cells].filter(element => element.classList.contains('category'));
-        const unit = [...cells].filter(element => element.classList.contains('unit'));
-        const cellData = unit.map((e, i) => {
-            const name = category[i].innerHTML;
-            const data = parseFloat(unit[i].innerHTML);
+    function getHeading(col) {
+        let n = thead.rows.length;
+        let i, headings = [], tr, th;
 
-            // console.log(`name ${name}`);
-            // console.log(`data ${data}`);
-            const array = new Array;
-            array.push({'name': name, 'data': data});
+        if (col < 0) {
+            return null;
+        }
 
-            console.log(`array ${JSON.stringify(array)}`);
+        for (i = 0; i < n; i++) {
+            tr = thead.rows[i];
+            if (tr.cells.length > col) {
+                th = tr.cells[col];
+                const data = th.innerText;
+                headings.push(data);
+            }
+        }
+        return headings;
+    }
 
-            return array;
-        });
+    function getCol(col) {
+        let n = tbody.rows.length;
+        let i, units = [], tr, td;
 
-        seriesData.push([].reduce.call(cellData, function(res, cell) {
-            return cell;
-        }, []));
-        return seriesData;
-    }, []);
+        if (col < 0) {
+            return null;
+        }
+
+        for (i = 0; i < n; i++) {
+            tr = tbody.rows[i];
+            if (tr.cells.length > col) {
+                td = tr.cells[col];
+                const data = parseFloat(td.innerText);
+                units.push(data);
+            }
+        }
+        return units;
+    }
 
     let seriesArray = new Array;
-    seriesArray.push(seriesData);
+    const unitLength = tbody.rows[0].cells.length;
+    for (let i = 1; i < unitLength; i++) {
+        const name = getHeading(i);
+        const data = getCol(i);
+        seriesArray.push({name: name, data: data});
+    }
 
     return seriesArray;
 }
