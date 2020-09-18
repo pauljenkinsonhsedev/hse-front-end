4ed0a9dddf15a5af76a2ac03d4e19b8944b9df92
Peter Broom <me@peterbroom.co.uk>
2020-09-18 19:34:56 +0100

    refactor default charts

--- src/shared/js/combined/charts/options-default.js
+++ src/shared/js/combined/charts/options-default.js
@@ -11,6 +11,34 @@ import { ChartOptions } from './dependencies';
 export class ChartOptionsDefault extends ChartOptions {
     constructor(container, collection){
         super(container, collection);
+        this.dataTable = container.querySelector('.tabledata');
+        this.defaults;
+       /*
+            Plot events boolean
+            - diables click events if only one set of data
+        */
+        for (let row of this.dataTable.rows) {
+            const cells = row.querySelectorAll('td');
+            if (cells.length > 2) {
+                this.plotEvents = true;
+            }
+        }
+
+        const plotOptions = {
+            series: {
+                showInLegend: true,
+                events: {
+                    legendItemClick: () => {
+                        return this.plotEvents;
+                    }
+                }
+            },
+            column: {
+                maxPointWidth: this.colWidth
+            }
+        };
+        const defaults = this.defaults;
+        this.collection = {...defaults, plotOptions};
     }
 
     init() {
