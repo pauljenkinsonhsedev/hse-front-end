08a22c04534adda5e300ff2aa8eb6a9dbd3a3105
Peter Broom <me@peterbroom.co.uk>
2020-09-21 16:39:52 +0100

    hex to rgba exported function

--- src/shared/js/combined/charts/plot-band.js
+++ src/shared/js/combined/charts/plot-band.js
@@ -1,3 +1,5 @@
+import { hex_to_rgba } from '../utils/hex-to-rgba';
+
 export function plotBand(data, colours) {
     const plotData = new Array;
     const thead = data.querySelector('.table__head');
@@ -19,7 +21,7 @@ export function plotBand(data, colours) {
             {
                 from: from,
                 to: to,
-                color: ahex_to_rba(colours[k], 0.3),
+                color: hex_to_rgba(colours[k], 0.3),
                 label: {
                     text: titleText,
                     align: 'left',
@@ -34,37 +36,4 @@ export function plotBand(data, colours) {
         );
     }
     return plotData;
-}
-
-function ahex_to_rba(ahex, opacity) {
-    //clean #
-    ahex = ahex.substring(1, ahex.length);
-    ahex = ahex.split('');
-
-    var r = ahex[0] + ahex[0],
-        g = ahex[1] + ahex[1],
-        b = ahex[2] + ahex[2],
-        a = ahex[3] + ahex[3];
-
-    if (ahex.length >= 6) {
-        r = ahex[0] + ahex[1];
-        g = ahex[2] + ahex[3];
-        b = ahex[4] + ahex[5];
-        a = ahex[6] + (ahex[7] ? ahex[7] : ahex[6]);
-    }
-
-    var int_r = parseInt(r, 16),
-        int_g = parseInt(g, 16),
-        int_b = parseInt(b, 16),
-        int_a = parseInt(a, 16);
-
-
-    int_a = int_a / 255;
-
-    if (int_a < 1 && int_a > 0) int_a = int_a.toFixed(2);
-
-    if (int_a || int_a === 0)
-        // return `rgba(, '+int_g+', '+int_b+', '+int_a+')`;
-        return `rgb(${int_r},${int_g},${int_b}, ${opacity})`;
-    return `rgb(${int_r},${int_g},${int_b}, ${opacity})`;
 }
