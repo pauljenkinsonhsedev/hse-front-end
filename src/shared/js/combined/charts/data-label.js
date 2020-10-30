caf4757afaf7660419095b71dcfc006042298f42
Peter Broom <me@peterbroom.co.uk>
2020-10-30 14:41:38 +0000

    data label prefix

--- src/shared/js/combined/charts/data-label.js
+++ src/shared/js/combined/charts/data-label.js
@@ -1,12 +1,17 @@
 import { displaySuffix } from './data-suffix.js';
+import { displayPrefix } from './data-prefix.js';
 
 export function dataLabel(units) {
     const dataLabelsSuffix = displaySuffix(units);
+    const dataLabelsPrefix = displayPrefix(units);
     let result = new String;
 
     switch (units) {
     case 'percentage':
         result = `<b>{point.name}</b>: {point.percentage:.1f}${dataLabelsSuffix}`;
+        break;
+    case 'pounds':
+        result = `{point.name}: <b>${dataLabelsPrefix}{point.y}</b>${dataLabelsSuffix}`;
         break;
     default:
         result = `{point.name}: <b>{point.y}${dataLabelsSuffix}</b>`;
