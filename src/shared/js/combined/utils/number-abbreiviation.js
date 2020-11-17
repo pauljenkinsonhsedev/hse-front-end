51a44b3889ffca086d886a10f4128cda73d96fe9
Peter Broom <me@peterbroom.co.uk>
2020-11-17 16:18:11 +0000

    parsefloat figures

--- src/shared/js/combined/utils/number-abbreiviation.js
+++ src/shared/js/combined/utils/number-abbreiviation.js
@@ -1,16 +1,16 @@
 export function abbreviateNumber(num) {
-    const SI_SYMBOL = ['', 'k', ' million', ' bn', 'T', 'P', 'E'];
+    const symbol = ['', 'k', ' million', ' bn', 'T', 'P', 'E'];
     const tier = Math.floor(Math.log10(num) / 3) || 0;
     let result = '' + num;
     // if zero, we don't need a suffix
     if (tier > 0) {
         // get suffix and determine scale
-        const suffix = SI_SYMBOL[tier];
+        const suffix = symbol[tier];
         const scale = Math.pow(10, tier * 3);
         // scale the number
         const scaled = num / scale;
         // format number and add suffix
-        result = scaled.toFixed(1).replace('.0', '') + suffix;
+        result = parseFloat(scaled.toFixed(2).replace('.0', '')) + suffix;
     }
     return result;
 }
