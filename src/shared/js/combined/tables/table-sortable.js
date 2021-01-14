6a810ff531449e9083f589d2858f847a5128502a
Peter Broom <me@peterbroom.co.uk>
2021-01-14 15:30:29 +0000

    sortable table logic

--- src/shared/js/combined/tables/table-sortable.js
+++ src/shared/js/combined/tables/table-sortable.js
@@ -1,63 +1,73 @@
 export function tableSortable(elem){
+    const tableHeaders = elem.querySelectorAll('thead th');
 
-    var table = document.querySelectorAll(`table.${elem.className}`);
+    // Sorting event
+    for (let i = 0; i < tableHeaders.length; i++) {
+        tableHeaders[i].addEventListener('click', function (e) {
+            sortTable(i);
+        });
+    };
 
-    table.forEach(() => {
-
-    });
-
-    var rows, i, x, y, count = 0;
-    var switching = true;
-
-    // Order is set as ascending
-    var direction = "ascending";
-
-    // Run loop until no switching is needed
-    while (switching) {
-        switching = false;
-        var rows = table.rows;
-
-        //Loop to go through all rows
-        for (i = 1; i < (rows.length - 1); i++) {
-            var Switch = false;
-
-            // Fetch 2 elements that need to be compared
-            x = rows[i].getElementsByTagName("TD")[0];
-            y = rows[i + 1].getElementsByTagName("TD")[0];
-
-            // Check the direction of order
-            if (direction == "ascending") {
-
-                // Check if 2 rows need to be switched
-                if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase())
-                    {
-                    // If yes, mark Switch as needed and break loop
-                    Switch = true;
-                    break;
-                }
-            } else if (direction == "descending") {
-
-                // Check direction
-                if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase())
-                    {
-                    // If yes, mark Switch as needed and break loop
-                    Switch = true;
-                    break;
+    // Table header event
+    body.addEventListener('click', event => {
+        tableHeaders.forEach(function callback(item) {
+            if (event.target !== item) {
+                item.classList.remove('sorted-asc');
+                item.classList.remove('sorted-desc');
+                return
+            } else {
+                if (!item.classList || item.classList.contains('sorted-desc')) {
+                    item.classList.remove('sorted-desc');
+                    !item.classList.contains('sorted-asc') ? item.classList.add('sorted-asc'): item.classList.remove('sorted-asc');
+                } else {
+                    item.classList.remove('sorted-asc');
+                    !item.classList.contains('sorted-desc') ? item.classList.add('sorted-desc'): item.classList.remove('sorted-desc');
                 }
             }
-        }
-        if (Switch) {
-            // Function to switch rows and mark switch as completed
-            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-            switching = true;
+        });
+    });
 
-            // Increase count for each switch
-            count++;
-        } else {
-            // Run while loop again for descending order
-            if (count == 0 && direction == "ascending") {
-                direction = "descending";
+    function sortTable(n) {
+        const table = elem.querySelector('tbody');
+
+        let i, x, y, count = 0;
+        let switching = true;
+
+        let direction = 'ascending';
+
+        while (switching) {
+            switching = false;
+            let rows = table.rows;
+
+            for (i = 0; i < (rows.length - 1); i++) {
+                var Switch = false;
+
+                x = rows[i].getElementsByTagName('td')[n];
+                y = rows[i + 1].getElementsByTagName('td')[n];
+
+                if (direction == 'ascending') {
+                    if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                        Switch = true;
+                        break;
+                    }
+                } else if (direction == 'descending') {
+                    if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                        Switch = true;
+                        break;
+                    }
+                }
+            }
+
+            if (Switch) {
+                rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
                 switching = true;
+
+                count++;
+            } else {
+                if (count == 0 && direction == 'ascending') {
+                    direction = 'descending';
+                    switching = true;
+                }
             }
         }
     }
