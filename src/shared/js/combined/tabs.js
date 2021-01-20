b09f1b8685bf2aad760c2e3ea2dfe1052a144b19
Peter Broom <me@peterbroom.co.uk>
2021-01-20 11:25:42 +0000

    target list item

--- src/shared/js/combined/tabs.js
+++ src/shared/js/combined/tabs.js
@@ -3,13 +3,14 @@ import { mediaQuery } from './utils/media-query.js';
 export function tabs(container) {
     const mediaquery = mediaQuery();
     const tabTitle = container.getElementsByClassName('tabs__tab');
+    const listItem = container.getElementsByClassName('tabs__list-item');
     const tabContent = container.getElementsByClassName('tabs__panel');
 
     if (tabTitle.length == 0 || tabTitle == undefined || tabTitle == "undefined") {
         return;
     }
 
-    tabTitle[0].classList.add('selected');
+    listItem[0].classList.add('selected');
     tabContent[0].classList.add('show');
 
     for (var i = 0; i < tabContent.length; i++) {
@@ -23,14 +24,14 @@ export function tabs(container) {
         }
 
         if (!tabTitle[i].classList.contains('selected')) {
-            [].forEach.call(container.querySelectorAll('.tabs__tab'), function (el) {
-                el.classList.remove("selected");
+            [].forEach.call(container.querySelectorAll('.tabs__list-item'), function (el) {
+                el.classList.remove('selected');
             });
             [].forEach.call(container.querySelectorAll('.tabs__panel'), function (el) {
-                el.classList.remove("show");
+                el.classList.remove('show');
             });
-            tabTitle[i].classList.add("selected");
-            tabContent[i].classList.add("show");
+            listItem[i].classList.add('selected');
+            tabContent[i].classList.add('show');
         }
     }
 }
