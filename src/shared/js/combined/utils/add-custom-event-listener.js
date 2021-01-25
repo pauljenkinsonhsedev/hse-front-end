2f9aecd6bc84e59c5037fde2d9d542605d6fab17
Peter Broom <me@peterbroom.co.uk>
2021-01-25 10:57:28 +0000

    custom event listner util

---
+++ src/shared/js/combined/utils/add-custom-event-listener.js
@@ -0,0 +1,15 @@
+export function customEventListener (selector, event, handler) {
+    let rootElement = document.querySelector('body');
+    rootElement.addEventListener(event, function (evt) {
+            var targetElement = evt.target;
+            while (targetElement != null) {
+                if (targetElement.matches(selector)) {
+                    handler(evt);
+                    return;
+                }
+                targetElement = targetElement.parentElement;
+            }
+        },
+        true
+    );
+}
