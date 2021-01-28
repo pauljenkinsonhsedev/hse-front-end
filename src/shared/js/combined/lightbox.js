3397fe78e1d60cb9d963ac44cbe188374317e54a
Peter Broom <me@peterbroom.co.uk>
2021-01-28 14:17:13 +0000

    refactor lightbox

--- src/shared/js/combined/lightbox.js
+++ src/shared/js/combined/lightbox.js
@@ -11,103 +11,121 @@ export function lightbox() {
         // overlay: true
     }
 
-    // select prev
-    function previous() {
-        let prev;
-        const panel = document.querySelectorAll('.lightbox__content--panel');
-        const length = panel.length;
-        const selected = [...panel].filter(element => element.classList.contains('active'));
-        const pos = parseInt(selected[0].dataset.pos);
+    // navigation
+    function navigation (e) {
+        let goTo = Number;
+        const lightbox = document.querySelector('.lightbox__content--panel');
+        const lightboxes = document.querySelectorAll('.lightbox__content--panel');
+        const selected = [...lightboxes].filter(element => element.classList.contains('active'));
+        const related = [...lightboxHandle].filter(element => element.rel === 'asbestos-gallery');
+        const length = related.length;
+        const pos = parseInt(lightbox.dataset.pos);
 
-        if (pos === 0) {
-            prev = length -1;
-        } else {
-            prev = pos -1;
+        const target = e.target;
+
+        if (target.classList.contains('prev')) {
+            if (pos === 0) {
+                console.log('current', pos);
+                goTo = length -1;
+            } else {
+                goTo = pos -1;
+            }
         }
 
-        displayPanel(pos, prev);
-    }
-
-    // select next
-    function next() {
-        let next;
-        const panel = document.querySelectorAll('.lightbox__content--panel');
-        const length = panel.length;
-        const selected = [...panel].filter(element => element.classList.contains('active'));
-        const pos = parseInt(selected[0].dataset.pos);
-
-        if (pos === (length -1)) {
-            next = 0;
-        } else {
-            next = pos + 1;
+        if (target.classList.contains('next')) {
+            if (pos === (length -1)) {
+                console.log('current', pos);
+                goTo = 0;
+            } else {
+                goTo = pos + 1;
+            }
         }
 
-        displayPanel(pos, next);
+        // console.log('selected', selected);
+        // console.log('related', related);
+        // console.log('related length', length);
+        console.log('goTo', goTo);
+
+        console.log('navigate');
+
+        const html = buildLightbox(goTo);
+        const modalContent = document.querySelector('.modal__content');
+        modalContent.innerHTML = html;
     }
 
     // lightbox handle event
     lightboxHandle.forEach(function (target, index) {
+        const rel = target.rel;
         target.addEventListener('click', function(e) {
             e.preventDefault();
-            const html = buildLightbox(index);
-
+            const html = buildLightbox(index, rel);
             new Modal(html, modalOptions);
         });
     });
 
     // prev next events
-    customEventListener('.lighbox__prev', 'click', previous);
-    customEventListener('.lighbox__next', 'click', next);
+    customEventListener('.lightbox__nav', 'click', navigation);
 
-    // select panel to display
-    function displayPanel(pos, index) {
-        const panel = document.querySelectorAll('.lightbox__content--panel');
-
-        panel[pos].classList.remove('active');
-        panel[index].classList.add('active');
-    }
+    // window.addEventListener('keydown', (e) => {
+    //     if (e.keyCode === 39 || e.keyCode === 37) {
+    //     console.log('nav');
+    //         navigation();
+    //     }
+    // }, false);
 
     // build collections
-    function buildLightbox(active) {
-        const mediaquery = mediaQuery();
-        let content = ``;
-        let caption = ``;
-        lightboxArray.forEach(function(elem, index) {
+    function buildLightbox(pos) {
+        const newImage = document.createElement('img');
+        const source = document.querySelectorAll('.lightbox a')[pos];
+        const sourceUrl = source.href;
+        const caption = source.title;
+        newImage.src = source.href;
+        newImage.alt = source.title;
+        let width = newImage.naturalWidth;
+        let height = newImage.naturalHeight;
 
-            const image = elem.querySelector('img');
-            const newImage = document.createElement('img');
-            let activeClass = ''
-            newImage.src = image.src;
-            newImage.alt = image.alt;
-            caption = elem.title;
+        if (width === 0) {
+            width = `100%`;
+        } else {
+            width = `${width}px`;
+        }
 
-            const width = image.naturalWidth;
+        if (height === 0) {
+            height = `100%`;
+        } else {
+            height = `${height}px`;
+        }
 
-            if (mediaquery !== 'small') {
-                elem.style.width = width + 'px';
-            }
+        // const headers = new Headers({'Access-Control-Allow-Origin': 'https://via.placeholder.com'});
+        // const imagePromise = fetch(sourceUrl, {
+        //     method: 'GET',
+        //     referrerPolicy: 'no-referrer',
+        //     headers: headers,
+        //     credentials: 'include',
+        //     mode: 'cors',
+        //     cache: 'reload'
+        // });
 
-            if (active === index) {
-                activeClass = 'active'
-            }
-
-            content = content + `<div class="lightbox__content--panel ${activeClass}" data-pos="${index}" data-caption="${image.alt}">${newImage.outerHTML}<div class="lighbox__caption"><p>${caption}</p></div></div>`;
-        });
+        // Promise
+        // .all([imagePromise])
+        // .then(responses => {
+        //     console.log('imagePromise',responses);
+        // })
+        // .catch(error => {
+        //     console.error(error);
+        // });
 
         return `
         <div class="lightbox__container">
-            <div class="lightbox__content">
-            ${content}
+            <div class="lightbox__content" style="width: ${width}; height: ${height};">
+                <div class="lightbox__content--panel" data-pos="${pos}" data-caption="${caption}">${newImage.outerHTML}</div>
             </div>
-            <div class="lighbox__footer">
-                <button class="lighbox__prev">
-                    <span>Prev</span>
-                </button>
-                <button class="lighbox__next">
-                    <span>Next</span>
-                </button>
+            <div class="lightbox__footer">
+                <button class="lightbox__nav prev" title="previous"></button>
+                <div class="lightbox__caption">${caption}</div>
+                <button class="lightbox__nav next" title="next"></button>
             </div>
         </div>
-        `
+        `;
     }
 }
