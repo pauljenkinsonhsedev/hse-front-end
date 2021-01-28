7a9e81226666bf7be09b179613389be01e5120a4
Peter Broom <me@peterbroom.co.uk>
2021-01-28 16:50:01 +0000

    dialog functionality

--- src/shared/js/combined/dialogs.js
+++ src/shared/js/combined/dialogs.js
@@ -1,5 +1,25 @@
+import Modal from './utils/modal';
+
 export function dialogModal(container) {
 
-    const content = container.querySelector('.dialog__content');
+    const dialogContainer = document.querySelectorAll('.dialog');
+
+    [...dialogContainer].forEach((elem) => {
+        console.log('dataset', elem.dataset);
+        const size = elem.dataset.size ? elem.dataset.size : 'default';
+        const content = elem.querySelector('.dialog__content').innerHTML;
+        const action = elem.querySelector('.dialog__action');
+        const modalOptions = {
+            size: size,
+            transition: true,
+            // overlay: true
+        };
+
+        console.log('modalOptions', modalOptions);
+
+        action.addEventListener('click', () => {
+            new Modal(content, modalOptions);
+        });
+    });
 
 }
