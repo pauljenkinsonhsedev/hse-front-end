807b5440418535b76dc570c7db810945c4eb995a
Peter Broom <me@peterbroom.co.uk>
2021-02-19 14:07:07 +0000

    remove comments

--- src/shared/js/combined/cookies/cookie-gtm.js
+++ src/shared/js/combined/cookies/cookie-gtm.js
@@ -18,9 +18,9 @@ function addGTM() {
 
     body.insertBefore(noscript, body.childNodes[0]);
 
+    load.css('./assets/v5-css/vendor/gtm.css');
     load.jsHead('./assets/v5-js/gtm-anti-flicker.js');
     load.jsHead('./assets/v5-js/gtm.js');
-    load.css('./assets/v5-css/vendor/gtm.css');
 
 }
 
@@ -41,66 +41,3 @@ export function cookiesGTM(status) {
         removeGTM();
     }
 }
-
-// var cookieVal = Cookies.get('cookies_policy');
-
-// const decodedCookieVal = window.atob(cookieVal);
-
-// const obj = JSON.parse(decodedCookieVal);
-
-// // Assigns variables to cookies values
-// var usageSet = (obj.usage);
-// // var campaignsSet = (obj.campaigns);
-// // var settingsSet = (obj.settings);
-// // console.log('settingsSet',settingsSet);
-
-// // When DOM is ready, create function for dynamically loading GTM scripts
-
-// function gtmFunction() {
-//     // Function adds script
-
-//     // Get the first script element on the page
-//     var ref = document.getElementsByTagName( 'head' )[0];
-
-//     // Create a new script element
-//     var gtm = document.createElement( 'script' );
-
-//     // Set attributes
-//     gtm.setAttribute("id", "gtm");
-
-//     // Set the script element `src`
-//     gtm.src = 'https://www.hse.gov.uk/assets/v5-js/gtm.js';
-
-//     // Inject the script into the DOM
-
-//     // ref.insertBefore( gtmAsync, ref.childNodes[0] );
-//     ref.insertBefore( gtm, ref.childNodes[0] );
-
-//     $('head').prepend('<style id="gtm-async-hide">.async-hide { opacity: 0 !important} </style>');
-//     $('body').prepend('<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PJPWMNL" height="0" width="0" style="display:none;visibility:hidden" aria-hidden="true"></iframe></noscript>');
-// }
-
-// $( document ).ready(function() {
-
-
-//     $('#acceptAllCookies').click(function() {
-//         // Call function on accept all cookies button (Cookie preferences banner)
-//         gtmFunction();
-//     });
-
-// // Checks to see if analytics preference is set to true
-
-//     if (usageSet == true )  {
-//         // Call function on accept all cookies button (Cookie preferences banner)
-//         gtmFunction();
-//     }  else { // If analytics preference is set to false - delete all GTM cookies
-//         Cookies.remove('_ga');
-//         Cookies.remove('_gid');
-//         Cookies.remove('_gali');
-//         Cookies.remove('_dc_gtm_UA-324220-1');
-//         Cookies.remove('_gat_UA-324220-1');
-//         Cookies.remove('_ga_1Y6RD6YT11');
-//         Cookies.remove('nmstat'); // Site Improve cookie - invoked by Google Analytics
-//     }
-
-// });
