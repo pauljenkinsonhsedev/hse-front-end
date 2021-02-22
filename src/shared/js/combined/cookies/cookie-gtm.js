d8799563acb5616eec9e55c98050a49933177acc
Peter Broom <me@peterbroom.co.uk>
2021-02-22 14:12:14 +0000

    remove add gtm

--- src/shared/js/combined/cookies/cookie-gtm.js
+++ src/shared/js/combined/cookies/cookie-gtm.js
@@ -3,25 +3,7 @@ import load from '../utils/asset-loader';
 import Cookies from 'js-cookie';
 
 function addGTM() {
-    const gtmID = 'GTM-PJPWMNL';
-
-    const body = document.getElementsByTagName('body')[0];
-    const noscript = document.createElement('noscript');
-    const frame = document.createElement('frame');
-    frame.src = `https://www.googletagmanager.com/ns.html?id=${gtmID}`;
-    frame.style.display = 'none';
-    frame.style.visibility = 'hidden';
-    frame.style.width = '0px';
-    frame.style.height = '0px';
-    frame.ariaHidden = true;
-    noscript.appendChild(frame);
-
-    body.insertBefore(noscript, body.childNodes[0]);
-
-    load.css('./assets/v5-css/vendor/gtm.css');
-    load.jsHead('./assets/v5-js/gtm-anti-flicker.js');
-    load.jsHead('./assets/v5-js/gtm.js');
-
+    // no longer required. Now in the html
 }
 
 function removeGTM() {
