85b4b4ffc70e4b334db013bea305805124aef536
Paul Jenkinson <pjenkinsonmail@gmail.com>
2021-03-09 10:23:03 +0000

    fix path to cookie page

--- src/shared/js/combined/cookies/cookie-banner-html.js
+++ src/shared/js/combined/cookies/cookie-banner-html.js
@@ -21,14 +21,14 @@ export function cookieMessageHTML(status) {
             `;
     const cookieMessageAccept = `
                 <div class="cookies-message__copy cookies-message-accepted">
-                    <p>You've accepted analytics cookies. You can change your <a href="cookies.htm">cookie settings</a> at any time.</p>
+                    <p>You've accepted analytics cookies. You can change your <a href="/cookies.htm">cookie settings</a> at any time.</p>
                     <button id="cookieNotifyClose" class="btn btn-cautionary">Hide</button>
                 </div>
             `;
 
     const cookieMessageReject = `
                 <div class="cookies-message__copy cookies-message-rejected">
-                    <p>You've rejected analytics cookies. You can change your <a href="cookies.htm">cookie settings</a> at any time.</p>
+                    <p>You've rejected analytics cookies. You can change your <a href="/cookies.htm">cookie settings</a> at any time.</p>
                     <button id="cookieNotifyClose" class="btn btn-cautionary">Hide</button>
                 </div>
             `;
