c6befdbc776376aea0ad3f3dc64be89607c722a0
HarleyBroom <harleybroom@gmail.com>
2021-08-18 17:12:24 +0100

    add formdata-polyfil to webpack

--- gulp/tasks/shared/webpack.config.dev.js
+++ gulp/tasks/shared/webpack.config.dev.js
@@ -6,8 +6,8 @@ module.exports = {
   devtool: 'source-map',
   target: 'web',
   entry: {
-    'v5-main.min': ['whatwg-fetch', './src/shared/js/v5-main.js'],
-    'v5-cookies': ['whatwg-fetch', './src/shared/js/v5-cookies.js'],
+    'v5-main.min': ['formdata-polyfill', 'whatwg-fetch', './src/shared/js/v5-main.js'],
+    'v5-cookies': ['formdata-polyfill', 'whatwg-fetch', './src/shared/js/v5-cookies.js'],
   },
   output: {
     filename: '[name].js',
