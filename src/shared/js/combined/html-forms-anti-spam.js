e9f89b25c2509ebb42a3daef90177952f9e7134f
PeterBroom <me@peterbroom.co.uk>
2021-10-11 15:42:23 +0100

    add flag in event no anti spam form on page

--- src/shared/js/combined/html-forms-anti-spam.js
+++ src/shared/js/combined/html-forms-anti-spam.js
@@ -2,47 +2,41 @@ export function htmlFormsAntiSpam() {
 
 
 const accordionFormButton = document.querySelector('.btn-form');
+  if (!accordionFormButton) {
+    return;
+  }
+  // Event listener for form button
 
-// Event listener for form button 
+  accordionFormButton.addEventListener('click', insertForm);
 
-accordionFormButton.addEventListener('click', insertForm);
+  function insertForm() {
 
-function insertForm() {
-    
-const formButton = document.querySelector('.btn-form');
+  const formButton = document.querySelector('.btn-form');
 
-if (!formButton) {
-  return;
+  if (!formButton) {
+    return;
+  }
+  const formContainer = document.querySelector('.form-content');
+  const content = formContainer.dataset.content;
+
+  // get html form
+  fetch(`/assets/ajax/forms/${content}.htm`).then(function (response) {
+    return response.text();
+    }).then(function (html) {
+
+    // form html string
+    const formhtml = (html);
+    formContainer.innerHTML = formhtml ;
+
+    }).catch(() => {
+        console.error(`No such file: ${content}`);
+    });
+
+    // scroll to content
+    formContainer.scrollIntoView({behavior: "smooth", block: "start"});
+
+    // remove form button
+    const contentContainer = document.querySelector('#contentContainer');
+    contentContainer.removeChild(accordionFormButton);
+  }
 }
-const formContainer = document.querySelector('.form-content');
-const content = formContainer.dataset.content;
-
-// get html form
-
-fetch(`/assets/ajax/forms/${content}.htm`).then(function (response) {
-return response.text();
-}).then(function (html) {
-
-// form html string    
-const formhtml = (html);
-formContainer.innerHTML = formhtml ;
-
-}).catch(() => {
-    console.error(`No such file: ${content}`);
-});
-
-// scroll to content
-formContainer.scrollIntoView({behavior: "smooth", block: "start"});
-
-// remove form button
-const contentContainer = document.querySelector('#contentContainer');
-contentContainer.removeChild(accordionFormButton);
-
-}
-
-}
-
-
-
-
-
