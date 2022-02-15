c32b7866372c5e327b55a9c81eebd13050a04f40
Paul Jenkinson <Paul.Jenkinson@hse.gov.uk>
2022-02-15 17:02:30 +0000

    function to load bowser

---
+++ src/shared/js/combined/bowser.js
@@ -0,0 +1,14 @@
+import load from './utils/asset-loader';
+import pathEnv from './utils/asset-env-path';
+
+
+export function bowser() {
+
+    return Promise.all([
+        load.js(pathEnv + "/assets/v5-js/vendor/bowser/es5.js"),
+    ])
+    .catch((err) => {
+        console.error(`Error initiating bowser: ${err}`);
+    });
+
+}
