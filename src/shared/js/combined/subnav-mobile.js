92ab632a9055f1fd3b28a0a76abe3cdb3b9f2ce2
Paul Jenkinson <Paul.Jenkinson@hse.gov.uk>
2022-10-10 19:46:08 +0100

    mobile toggle menu
    adds toggle function for mobile menu, includes tab-indexing for accessibility

--- src/shared/js/combined/subnav-mobile.js
+++ src/shared/js/combined/subnav-mobile.js
@@ -1,100 +1,148 @@
+import { mediaQuery } from './utils/media-query.js';
 
 export function subNavMobile() {
 
-// Handler that uses various data-* attributes to trigger
-// specific actions, mimicing bootstraps attributes
-
-// Grab all the trigger elements on the page
-const triggers = Array.from(document.querySelectorAll('[data-toggle="collapse"]'));
-	
-
-	
-	
-	
-// Listen for click events, but only on our triggers
-window.addEventListener('click', (evnt) => {
-	
-  const elm = evnt.target;
-  if (triggers.includes(elm)) {
-	  
-
-	  
-	  
-    const selector = elm.getAttribute('data-target');	 
-	  
-    collapse(selector, 'toggle');
-  }
-}, false);
-
-// map our commands to the classList methods
-const fnmap = {
-  'toggle': 'toggle',	// toggle
-  'show': 'add',		// add
-  'hide': 'remove'		// remove
-
-};
-
-function collapse(selector, command) {
-	
-  const targets = Array.from(document.querySelectorAll(selector));
-  targets.forEach(target => {
-    target.classList[fnmap[command]]('show');	// show
-	console.table(target.classList);
-  });
-	
-	
-	
-}
-
-// Mobile menu toggle
-
 const mobileButton = document.querySelector('.btn-mobile');
-// const mobileSVG = document.querySelector("[data-menu-icon=menu]");
+const secondaryMenu = document.querySelector('#menu');
 
-const mobileButtonState = mobileButton.getAttribute('aria-pressed');
-const listItem = document.querySelectorAll('.subnav-wrapper li');
+const listItem = document.querySelector('.subnav-wrapper');
+const listItems = listItem.querySelectorAll('li');
 
 // Set tab-index to -1 before click event (menu closed)
-if (mobileButtonState === 'false') {
-	listItem.forEach((item) => {
-		item.querySelector('a').tabIndex = -1;
+
+const mediaquery = mediaQuery();
+	if (mediaquery !== 'large') {
+
+	listItems.forEach((item) => {
+
+		const tabIndex = item.querySelector('a').tabIndex;
+		console.log(tabIndex);
+
+		if (tabIndex === 0 ) {
+			item.querySelector('a').tabIndex = -1;
+			item.classList.add("tab-index-0-page-load");
+
+		} else if (tabIndex === -1 ) {
+			item.querySelector('a').tabIndex = -1;	
+			item.classList.add("tab-index-1-page-load");
+
+		}
+
 	});
-}
+
+// Reset tab-index on first button click
+function onClick(event){
+	mobileButton.removeEventListener('click', onClick);
+	listItems.forEach((item) => {
+
+		if (item.classList.contains('tab-index-0-page-load')) {
+			item.querySelector('a').tabIndex = 0;
+		}
+
+		if (item.classList.contains('tab-index-1-page-load')) {
+			item.querySelector('a').tabIndex = -1;
+		}
+	});
+ }
+ 
+ mobileButton.addEventListener('click', onClick);
+} 
+
+
+
+ const buttonText = document.querySelector('.menu-mobile-text');
+ const svgPath = document.querySelector('#menu-mobile-path');
+
+ // Visually hide menu title
+ const menuTitle = document.querySelector('li.first').innerText;
+ buttonText.innerHTML = 'Open ' + '<span class="visually-hidden">' + menuTitle + '</span>' + ' menu';
+
+
+
 
 // Set attributes toggle
 function toggleIsOpen() {
+
+	// Get menu state
 	const currentState = mobileButton.getAttribute('aria-pressed');
-	const buttonText = document.querySelector('.menu-mobile-text');
+
+	// Tab indexing
+	const listItem = document.querySelector('.subnav-wrapper');
+    const listItems = listItem.querySelectorAll('li');
+    
+	// Set state to closed
 	mobileButton.setAttribute('aria-pressed', currentState === 'false');
-	const svgPath = document.querySelector('#menu-mobile-path');
 
 	if (currentState === 'false') {
-		buttonText.innerHTML = 'Close menu';
+
+		// SHOW MENU
 		mobileButton.setAttribute('aria-expanded', true);
+		secondaryMenu.classList.add('show');
+
 		// SVG attributes
 		svgPath.setAttribute('d', 'M18 7L7 18M7 7L18 18'); 
 		svgPath.setAttribute('stroke', 'white'); 
         svgPath.setAttribute('stroke-width', '1.2'); 
-		listItem.forEach((item) => {
-			item.querySelector('a').tabIndex = 0;
-			
+
+		// Button text
+
+		buttonText.innerHTML = 'Close ' + '<span class="visually-hidden">' + menuTitle + '</span>' + ' menu';
+
+		//  Open tab-index fix
+
+		listItems.forEach((item) => {
+
+			if (item.classList.contains('close-state-0')) {
+				item.querySelector('a').tabIndex = 0;
+			}
+	
+			if (item.classList.contains('close-state-1')) {
+				item.querySelector('a').tabIndex = -1;
+			}
 		});
 
+
 	} else {
-		buttonText.innerHTML = 'Open menu';
+
+		// HIDE MENU
+		secondaryMenu.classList.remove('show');
 		mobileButton.setAttribute('aria-expanded', false);
+
 		// SVG attributes
 		svgPath.setAttribute('d', 'M6 8.1H19V6.9H6V8.1ZM6 18.1H19V16.9H6V18.1ZM6 13.1H19V11.9H6V13.1Z');
 		svgPath.removeAttribute('stroke', 'white'); 
         svgPath.removeAttribute('stroke-width', '1.2'); 
-        listItem.forEach((item) => {
-			item.querySelector('a').tabIndex = -1;
+
+		// Button text
+
+		buttonText.innerHTML = 'Open ' + '<span class="visually-hidden">' + menuTitle + '</span>' + ' menu';
+
+		// Close tab-index fix
+
+		listItems.forEach((item) => {
+
+			const tabIndex = item.querySelector('a').tabIndex;
+			console.log(tabIndex);
+	
+			if (tabIndex === 0 ) {
+				item.querySelector('a').tabIndex = -1;
+				item.classList.add("close-state-0");
+				item.classList.remove("close-state-1");
+
+	
+			} else if (tabIndex === -1 ) {
+				item.querySelector('a').tabIndex = -1;	
+				item.classList.add("close-state-1");
+				item.classList.remove("close-state-0");
+
+	
+			}
+	
 		});
+
 
 	}
   }
   
   mobileButton.addEventListener('click', toggleIsOpen)
-
 };
-
