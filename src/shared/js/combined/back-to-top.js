a49b371c47eecd4dd3ed853d271e5644ad404477
Paul Jenkinson <Paul.Jenkinson@hse.gov.uk>
2024-05-09 12:28:06 +0100

    remove contents list only conditional
    back-to-top link will always display in Two Thirds / One Third layout

--- src/shared/js/combined/back-to-top.js
+++ src/shared/js/combined/back-to-top.js
@@ -2,12 +2,16 @@ export function backToTop() {
 
     var backToTop = document.querySelector('.hse-back-to-top__container');
     var backToTopHook = document.querySelector('.hse-back-to-top');
-    var contents = document.querySelector('.hse-contents-list');
+    // var contents = document.querySelector('.hse-contents-list');
 
-    // visually hides back-to-top link if there is not contents list
+    /* 
+
+    visually hides back-to-top link if there is not contents list
+
     if(!contents) {
     backToTopHook.classList.add("visually-hidden");
     }
+    */
 
     // get aside content
     let aside = document.querySelector('#contentAside');
