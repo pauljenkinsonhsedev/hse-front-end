28b66b941a30a6c645066fef08804f3870a69c2f
Paul Jenkinson <Paul.Jenkinson@hse.gov.uk>
2024-05-15 13:56:04 +0100

    refactor tabs JS to work with NHS class names

--- src/shared/js/combined/tabs.js
+++ src/shared/js/combined/tabs.js
@@ -11,9 +11,8 @@ export function tabs(container) {
     }
 
     // initial aria and tabindex
-
-    listItem[0].classList.add('selected');
-    tabContent[0].classList.add('show');
+    tabContent[0].classList.remove('hse-tabs__panel--hidden');
+    listItem[0].classList.add('hse-tabs__list-item--selected');
     tabTitle[0].setAttribute("aria-selected", "true");
     tabTitle[0].setAttribute("tabindex", "0");
 
@@ -29,18 +28,18 @@ export function tabs(container) {
 
         if (!tabTitle[i].classList.contains('selected')) {
             [].forEach.call(container.querySelectorAll('.hse-tabs__list-item'), function (el) {
-                el.classList.remove('selected');
+                el.classList.remove('hse-tabs__list-item--selected');
             });
             [].forEach.call(container.querySelectorAll('.hse-tabs__panel'), function (el) {
-                el.classList.remove('show');
+                el.classList.add('hse-tabs__panel--hidden');
             });
             [].forEach.call(container.querySelectorAll('.hse-tabs__tab'), function (el) {
                 // aria-selected and tabindex
                 el.setAttribute("aria-selected", "false");
                 el.setAttribute("tabindex", "-1");
             });
-            listItem[i].classList.add('selected');
-            tabContent[i].classList.add('show');
+            listItem[i].classList.add('hse-tabs__list-item--selected');
+            tabContent[i].classList.remove('hse-tabs__panel--hidden');
             // aria-selected and tabindex
             tabTitle[i].setAttribute("aria-selected", "true");
             tabTitle[i].setAttribute("tabindex", "0");
