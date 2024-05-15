5389e31a163898313d8dbc214fa80d06a66aff12
Paul Jenkinson <Paul.Jenkinson@hse.gov.uk>
2024-05-15 11:46:39 +0100

    add aria-selected and tabindex attributes

--- src/shared/js/combined/tabs.js
+++ src/shared/js/combined/tabs.js
@@ -10,8 +10,12 @@ export function tabs(container) {
         return;
     }
 
+    // initial aria and tabindex
+
     listItem[0].classList.add('selected');
     tabContent[0].classList.add('show');
+    tabTitle[0].setAttribute("aria-selected", "true");
+    tabTitle[0].setAttribute("tabindex", "0");
 
     for (var i = 0; i < tabContent.length; i++) {
         tabTitle[i].addEventListener('click', showHideTabs.bind(null, i));
@@ -30,8 +34,16 @@ export function tabs(container) {
             [].forEach.call(container.querySelectorAll('.tabs__panel'), function (el) {
                 el.classList.remove('show');
             });
+            [].forEach.call(container.querySelectorAll('.tabs__tab'), function (el) {
+                // aria-selected and tabindex
+                el.setAttribute("aria-selected", "false");
+                el.setAttribute("tabindex", "-1");
+            });
             listItem[i].classList.add('selected');
             tabContent[i].classList.add('show');
+            // aria-selected and tabindex
+            tabTitle[i].setAttribute("aria-selected", "true");
+            tabTitle[i].setAttribute("tabindex", "0");
         }
     }
 }
