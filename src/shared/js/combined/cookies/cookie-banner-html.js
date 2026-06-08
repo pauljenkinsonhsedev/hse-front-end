9f5f73c413bbf66094e4dfdae80abd490e3d1a2c
Paul Jenkinson <Paul.Jenkinson@hse.gov.uk>
2026-06-08 11:54:34 +0100

    apply hse-button--small to cookie banner action buttons

--- src/shared/js/combined/cookies/cookie-banner-html.js
+++ src/shared/js/combined/cookies/cookie-banner-html.js
@@ -1,8 +1,7 @@
-
 export function cookieMessageHTML() {
-    const status = Cookies.get('cookies_status');
+  const status = Cookies.get("cookies_status");
 
-    const cookieMessageStart = `
+  const cookieMessageStart = `
                 <div class="cookies-message">
                     <p class="cookies-message__heading hse-heading-m">Cookies on www.hse.gov.uk</p>
                     <div class="cookies-message__copy">
@@ -11,45 +10,44 @@ export function cookieMessageHTML() {
                     </div>
 
                     <div class="cookies-message__actions">
-                        <button class="hse-button" id="acceptAllCookies" href="#">Accept analytics cookies</button>
-                        <button class="hse-button" id="rejectAllCookies" href="#">Reject analytics cookie</button>
+                        <button class="hse-button hse-button--small" id="acceptAllCookies" href="#">Accept analytics cookies</button>
+                        <button class="hse-button hse-button--small" id="rejectAllCookies" href="#">Reject analytics cookie</button>
 
                         <p class="cookies-message__link"><a href="/help/cookies.htm">View cookies</a></p>
                     </div>
                 </div>
             `;
-    const cookieMessageAccept = `
+  const cookieMessageAccept = `
                 <div class="cookies-message__copy cookies-message-accepted">
                     <p>You've accepted analytics cookies. You can change your <a href="/help/cookies.htm">cookie settings</a> at any time.</p>
                     <button id="cookieNotifyClose" class="hse-button">Hide</button>
                 </div>
             `;
 
-    const cookieMessageReject = `
+  const cookieMessageReject = `
                 <div class="cookies-message__copy cookies-message-rejected">
                     <p>You've rejected analytics cookies. You can change your <a href="/help/cookies.htm">cookie settings</a> at any time.</p>
                     <button id="cookieNotifyClose" class="hse-button">Hide</button>
                 </div>
             `;
 
+  let cookieMessage;
+  switch (status) {
+    case "accepted":
+      cookieMessage = cookieMessageAccept;
+      break;
+    case "rejected":
+      cookieMessage = cookieMessageReject;
+      break;
+    default:
+      cookieMessage = cookieMessageStart;
+      break;
+  }
 
-    let cookieMessage;
-    switch (status) {
-        case 'accepted':
-            cookieMessage = cookieMessageAccept;
-            break;
-        case 'rejected':
-            cookieMessage = cookieMessageReject;
-            break;
-        default:
-            cookieMessage = cookieMessageStart
-            break;
-    }
-
-    const cookieBanner = `
+  const cookieBanner = `
     <div class="hse-cookie-banner cf" role="region" aria-label="Cookie banner">
         <div class="hse-width-container cf">${cookieMessage}</div>
     </div>
     `;
-    return cookieBanner;
+  return cookieBanner;
 }
