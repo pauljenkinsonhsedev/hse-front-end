e179db035fe85822f5f5d95d42519aa7a99389ce
Paul Jenkinson <Paul.Jenkinson@hse.gov.uk>
2026-06-15 17:23:05 +0100

    add polling to sass watchers for more reliable change detection

--- scripts/watch-ds.mjs
+++ scripts/watch-ds.mjs
@@ -9,6 +9,7 @@ const proc = spawn(
   'sass',
   [
     '--watch',
+    '--poll',
     `./src/secureroot/v6-css/scss/design-system.scss:${output}`,
     '--load-path=node_modules',
     '--style=compressed',
