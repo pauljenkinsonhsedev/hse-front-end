84dd82e74212c1bbf099ffd286ddd7cca8bef2bb
Paul Jenkinson <Paul.Jenkinson@hse.gov.uk>
2026-06-18 10:04:17 +0100

    replace information banner function with global banner function in main

--- src/shared/js/main.js
+++ src/shared/js/main.js
@@ -9,7 +9,7 @@ import { tabs } from "./combined/tabs.js";
 import { ariaLabels } from "./combined/aria-labels.js";
 import { googleSearch } from "./combined/google-search.js";
 import { topTasks } from "./combined/top-tasks.js";
-import { informationBanner } from "./combined/information-banner.js";
+import { hseBanner } from "./combined/banner.js";
 import { accordion } from "./combined/accordion.js";
 import { backToTop } from "./combined/back-to-top.js";
 import { dsSiteNavigation } from "./combined/ds-site-navigation.js";
@@ -35,7 +35,7 @@ window.addEventListener("DOMContentLoaded", () => {
 
   // Initialize Components
   accordion();
-  informationBanner();
+  hseBanner();
   topTasks();
   ariaLabels();
   tabs();
